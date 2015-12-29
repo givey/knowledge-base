eb4c5ca621c430623762cace06808a2bfbd1047a
cjmarkham <doddsey65@gmail.com>
2015-12-29 15:40:43 +0000

    Update api docs

--- _plugins/pretty_json.rb
+++ _plugins/pretty_json.rb
@@ -1,7 +1,11 @@
 module Jekyll
   module JsonFilter
     def pretty_json(input)
-      JSON.pretty_generate(input).gsub('<', '&lt;').gsub("\n", "<br/>")
+      JSON.pretty_generate(input).gsub("\n", "\r\n")
+    end
+
+    def json(input)
+      input.to_json
     end
   end
 end
